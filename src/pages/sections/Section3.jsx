97c010cca27498d16d2246d4ef4cd41980a47c85
titosco <titobiloluwatella@gmail.com>
2025-07-28 17:58:38 +0100

    progress

--- src/pages/sections/Section3.jsx
+++ src/pages/sections/Section3.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Section3 = () => {
   return (
-    <div>Section3</div>
+    <div className='p-5 px-3 flex flex-col md:flex-row justify-center gap-4 md:gap-16 lg:40 items-center top-0 w-full z-20 bg-gray-200 lg:px-26'>
+        {/* 1st text */}
+        <div className='flex gap-2 items-center'>
+            <span className='font-bold text-lg lg:text-4xl'>One Stop. <br /> Four <span className='text-primary'>Possibilities</span>.</span>
+        </div>
+        {/* remaining text */}
+        <div>
+            <ul className='items-center justify-center text-xs  flex flex-wrap gap-6 lg:gap-10 lg:text-lg'>
+                <li className='font-bold'>3M <h4 className='font-normal text-xs sm:text-sm'>Active users</h4></li>
+                <li className='font-bold'>60M <h4 className='font-normal text-xs sm:text-sm'>Links & QR <br /> codes created</h4></li>
+                <li className='font-bold'>1B <h4 className='font-normal text-xs sm:text-sm'>Clicked & Scanned <br /> connections</h4></li>
+                <li className='font-bold'>300K <h4 className='font-normal text-xs sm:text-sm'>App Integrations</h4></li>
+            </ul>
+        </div>
+    </div>
   )
 }
 
